3414915b23203c1a6dd6d70fc534883f7d7e7ff5
xajil <xajil.developer@gmail.com>
2015-04-07 15:46:07 -0600

    Carga Inicial
    Carga Inicial a Github de Java Compartido Pijatz

---
+++ src/javaXajil/TestInicial.java
@@ -0,0 +1,13 @@
+package javaXajil;
+
+public class TestInicial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo Git");
+	}
+
+}
