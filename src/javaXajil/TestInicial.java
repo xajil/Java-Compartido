002ca3ef085a5ee5d2ed0c633d7e1262a17d4604
xajil <xajil.developer@gmail.com>
2015-05-21 11:57:55 -0600

    Cambio en test incial a hola mundo

--- src/javaXajil/TestInicial.java
+++ src/javaXajil/TestInicial.java
@@ -7,7 +7,7 @@ public class TestInicial {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo Git");
+		System.out.println("Hola Mundo Git 2015");
 	}
 
 }
